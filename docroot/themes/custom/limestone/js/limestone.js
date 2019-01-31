412dac9ff7a17e5ddcbf69dbd4a97f51c83a95aa
kevncobb <kevncobb@gmail.com>
2019-01-31 14:28:13 -0500

    added to-top button

--- docroot/themes/custom/limestone/js/limestone.js
+++ docroot/themes/custom/limestone/js/limestone.js
@@ -44,4 +44,70 @@
       $(this).toggleClass("expander-hidden");
     });
   });
+  Drupal.behaviors.to_top = {
+    attach: function (context, settings) {
+      // Execute code once the DOM is ready. $(document).ready() not required within Drupal.behaviors.
+
+      (function (jQuery) {
+        jQuery.mark = {
+          jump: function (options) {
+            var defaults = {
+              selector: 'a.anchor-link'
+            };
+            if (typeof options == 'string') {
+              defaults.selector = options;
+            }
+
+            options = jQuery.extend(defaults, options);
+            return jQuery(options.selector).click(function (e) {
+              var jumpobj = jQuery(this);
+              var target = jumpobj.attr('href');
+              var thespeed = 1000;
+              var offset = jQuery(target).offset().top;
+              jQuery('html,body').animate({
+                scrollTop: offset
+              }, thespeed, 'swing');
+              e.preventDefault();
+            });
+          }
+        };
+      })(jQuery);
+
+
+      // To Top button appear on scroll
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+          $('#to-top:hidden').stop(true, true).fadeIn();
+        } else {
+          $('#to-top').stop(true, true).fadeOut();
+        }
+        if ($(this).scrollTop() > 400) {
+          $('.scroll-fade-1').stop(true, true).css("display", "none");
+          $('.scroll-fade-in-1:hidden').stop(true, true).fadeIn();
+        } else {
+          $('.scroll-fade-1:hidden').stop(true, true).fadeIn();
+          $('.scroll-fade-in-1').stop(true, true).css("display", "none");
+        }
+      });
+
+      jQuery(function(){
+        jQuery.mark.jump();
+      });
+
+
+      $(window).load(function () {
+        // Execute code once the window is fully loaded.
+
+      });
+
+      $(window).resize(function () {
+        // Execute code when the window is resized.
+      });
+
+      $(window).scroll(function () {
+        // Execute code when the window scrolls.
+      });
+
+    }
+  };
 })(jQuery, Drupal);
