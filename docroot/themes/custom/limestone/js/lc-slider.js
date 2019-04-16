25533a178388ed02d32307341f21a1feb00ebfea
kevncobb <kevncobb@gmail.com>
2019-04-16 14:19:20 -0400

    attached lc-slider library to template

--- docroot/themes/custom/limestone/js/lc-slider.js
+++ docroot/themes/custom/limestone/js/lc-slider.js
@@ -1,26 +1,14 @@
-(function (Drupal, $, window) {
-  Drupal.behaviors.lc_slider = {
-    attach: function (context, settings) {
-      // Execute code once the DOM is ready. $(document).ready() not required
-      // within Drupal.behaviors.
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
 
-      $(context).find('div.lc-slider');
+  /**
+   * Use this behavior as a template for custom Javascript.
+   */
+  
+})(jQuery, Drupal);
 
-      //$.map( $('.lc-hero-next-slide ul > li'), function (element) { return
-      // $(element).text() });
 
-      var titles = $('.lc-slide-next ul > li').map(function(i, el) {
-        return $(el).text();
-      }).get();
-      console.log(titles);
-      $( ".slideshow-controls a.next-headline" ).each(function(i) {
-        if ($(titles[i+1]).length) {
-          $(this).append(titles[0]);
-        } else {
-          $(this).append(titles[i+1]);
-        }
-      });
-
-    }
-  }
-}(Drupal, jQuery, this));
