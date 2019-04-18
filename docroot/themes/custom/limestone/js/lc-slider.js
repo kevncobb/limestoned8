0f520e098eda15d384de482af371a74806c436d9
kevncobb <kevncobb@gmail.com>
2019-04-18 09:50:01 -0400

    moved js from openedu installation

--- docroot/themes/custom/limestone/js/lc-slider.js
+++ docroot/themes/custom/limestone/js/lc-slider.js
@@ -5,23 +5,25 @@
  */
 (function ($, Drupal) {
 
+  Drupal.behaviors.lc_select = {
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  $(window).bind("load", function() {
-    var titles = $('.lc-slide-next ul > li').map(function(i, el) {
-      return $(el).text();
-    }).get();
-    consoleLog(titles);
-    $( ".slideshow-controls a.next-headline" ).each(function(i) {
-      if (titles[i+1] == null) {
-        $(this).append(titles[0]);
-      } else {
-        $(this).append(titles[i+1]);
-      }
+    $(window).bind("load", function() {
+      var titles = $('.lc-slide-next ul > li').map(function(i, el) {
+        return $(el).text();
+      }).get();
+      consoleLog(titles);
+      $( ".slideshow-controls a.next-headline" ).each(function(i) {
+        if (titles[i+1] == null) {
+          $(this).append(titles[0]);
+        } else {
+          $(this).append(titles[i+1]);
+        }
+      });
     });
-  });
-
+  };
 })(jQuery, Drupal);
+
 
 
