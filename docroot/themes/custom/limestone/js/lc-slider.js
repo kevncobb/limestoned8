6abf48cb9e4ddcc95724290909a815a7adb4c0c5
kevncobb <kevncobb@gmail.com>
2019-04-18 15:07:00 -0400

    fixed lc-slider & cover-image > div height

--- docroot/themes/custom/limestone/js/lc-slider.js
+++ docroot/themes/custom/limestone/js/lc-slider.js
@@ -12,19 +12,14 @@
         var titles = $('.lc-slide-next ul > li').map(function(i, el) {
           return $(el).text();
         }).get();
-        console.log(titles.length);
 
         $( ".slideshow-controls a.next-headline" ).each(function(i) {
-          console.log(i + ' before if statement');
           if (i == (titles.length - 1) ) {
             $(this).append(titles[0]);
-            console.log(i + ' in if');
           } else {
-            console.log(i + ' in else');
             $(this).append(titles[i+1]);
           }
         });
-
       });
     }
   };
