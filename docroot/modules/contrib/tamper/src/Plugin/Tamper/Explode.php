f821c9c8ac6454969b33da0042801f3a6a1913ad
kevncobb <kevncobb@gmail.com>
2019-04-25 08:37:21 -0400

    testing patch on implode tamper plugin

--- docroot/modules/contrib/tamper/src/Plugin/Tamper/Explode.php
+++ docroot/modules/contrib/tamper/src/Plugin/Tamper/Explode.php
@@ -76,6 +76,9 @@ class Explode extends TamperBase {
    * {@inheritdoc}
    */
   public function tamper($data, TamperableItemInterface $item = NULL) {
+    if ($data == NULL) {
+      return FALSE;
+    }
     if (!is_string($data)) {
       throw new TamperException('Input should be a string.');
     }
