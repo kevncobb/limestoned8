c654039893f60f184720c6bac2353b4034b49d3f
kevncobb <kevncobb@gmail.com>
2020-06-11 15:23:32 -0400

    added readmore library

--- docroot/themes/custom/limestone/js/limestone.js
+++ docroot/themes/custom/limestone/js/limestone.js
@@ -8,15 +8,7 @@
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  Drupal.behaviors.trunk = {
-    attach: function (context, settings) {
-      // Using once() to apply the myCustomBehaviour effect when you want to do just run one function.
-      $(context).find('div.trunk').trunk8({
-        fill: '&hellip;',
-        lines: 6
-      });
-    }
-  };
+  
   Drupal.behaviors.callout = {
     attach: function (context, settings) {
       // Using once() to apply the myCustomBehaviour effect when you want to do just run one function.
