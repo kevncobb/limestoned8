de8593dcb9d83a6dee2711d065052f29e60c06ba
kevncobb <kevncobb@gmail.com>
2020-06-12 11:17:50 -0400

    updated readmore function

--- docroot/themes/custom/limestone/js/readmore.js
+++ docroot/themes/custom/limestone/js/readmore.js
@@ -5,47 +5,53 @@
  */
 (function ($, Drupal) {
 
-  Drupal.behaviors.readmore = {
-    attach: function (context, settings) {
-      $(document).ready(function() {
-        // Configure/customize these variables.
-        var showChar = 250;  // How many characters are shown by default
-        var ellipsestext = "...";
-        var moretext = "Show more >";
-        var lesstext = "Show less";
+  function someElementWatcher(context) {
+    // $.once acts like $.each, and loops through the found elements.
+    // The code inside $.once() will act on each element in the jQuery object
+    // a single time.
+    $(context).find(".readmore").once("readmore").each(function () {
+      // Configure/customize these variables.
+      var showChar = 250;  // How many characters are shown by default
+      var ellipsestext = "...";
+      var moretext = "Show more >";
+      var lesstext = "Show less";
 
 
-        $('.readmore').one(function() {
-          var content = $(this).html();
+      $('.readmore').one(function() {
+        var content = $(this).html();
 
-          if(content.length > showChar) {
+        if(content.length > showChar) {
 
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
+          var c = content.substr(0, showChar);
+          var h = content.substr(showChar, content.length - showChar);
 
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+          var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
-            $(this).html(html);
-          }
+          $(this).html(html);
+        }
 
-        });
-
-        $(".morelink").click(function(){
-          if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-          } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-          }
-          $(this).parent().prev().toggle();
-          $(this).prev().toggle();
-          return false;
-        });
       });
 
+      $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+          $(this).removeClass("less");
+          $(this).html(moretext);
+        } else {
+          $(this).addClass("less");
+          $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+      });
+    });
+      // $(this) refers to the current instance of $(".some_element")
 
+  }
+
+  Drupal.behaviors.readmore = {
+    attach:function (context) {
+      someElementWatcher();
     }
   };
-
-})(jQuery, Drupal);
+}(jQuery, Drupal));
