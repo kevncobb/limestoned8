7bbd273c4664d64dac0117deacfd2105f9147513
kevncobb <kevncobb@gmail.com>
2020-07-23 15:18:11 -0400

    updated site-alert js

--- docroot/themes/custom/limestone/js/alert.js
+++ docroot/themes/custom/limestone/js/alert.js
@@ -1,7 +1,49 @@
 /**
  * @file
- * Placeholder file for custom sub-theme behaviors.
- *
+ * Contains javascript to refresh alert div contents.
  */
 
+(function ($, Drupal, drupalSettings) {
 
+  let callback;
+
+  // Function to update alert text.
+  let loadAlert = function (siteAlert) {
+    // Object contains information about the currently loaded theme for
+    // processing by our theme callback. Without it the default theme is always
+    // assumed.
+    let options = {
+      ajax_page_state: drupalSettings.ajaxPageState
+    };
+    siteAlert.load(callback, options);
+
+    if (drupalSettings.siteAlert.timeout > 0) {
+      setTimeout(function () {
+        loadAlert(siteAlert);
+      }, drupalSettings.siteAlert.timeout * 1000);
+    }
+  };
+
+  Drupal.behaviors.siteAlert = {
+    attach: function (context, settings) {
+      // Work around a core bug that prevents cached pages from being
+      // invalidated in time for scheduled alerts to appear. Immediately load
+      // the alerts.
+      // @todo Remove this workaround once the bug is fixed.
+      // @see https://www.drupal.org/project/site_alert/issues/3121988
+      if (drupalSettings.siteAlert.workaround_needed) {
+        callback = settings.path.baseUrl + 'ajax/site_alert';
+        loadAlert($('.alert-section', context));
+      }
+
+      // Update content at configured interval.
+      if (drupalSettings.siteAlert.timeout > 0) {
+        callback = settings.path.baseUrl + 'ajax/site_alert';
+        setTimeout(function () {
+          loadAlert($('.alert-section', context));
+        }, drupalSettings.siteAlert.timeout * 1000);
+      }
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
