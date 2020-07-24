df9e551303178868f97b7bb71570501c26c03051
kevncobb <kevncobb@gmail.com>
2020-07-24 10:37:07 -0400

    added icon when closing site alert to top menu in order to open it again

--- docroot/themes/custom/limestone/js/alert.js
+++ docroot/themes/custom/limestone/js/alert.js
@@ -19,7 +19,7 @@
       $(context).find('.alert-section a.close').bind('touchstart click', function (event) {
         event.stopPropagation();
         event.preventDefault();
-        $( "<li class='top-menu-alert-icon'><a href='#'>" + alert_icon + "</a></li>" ).insertBefore( top_menu_first );
+        $( "<li class='top-menu-alert-icon'><a href='#'>" + alert_icon + "</a></li>" ).detach().insertBefore( top_menu_first );
         alert_section.addClass("d-none");
         var inHalfADay = 0.5;
         Cookies.set('alert', 'true', {
