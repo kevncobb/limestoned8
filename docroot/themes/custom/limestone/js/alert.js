94a681b396fe6e72592c2c31a594e7e0c57b16ad
kevncobb <kevncobb@gmail.com>
2020-08-03 11:21:23 -0400

    updated alert functions

--- docroot/themes/custom/limestone/js/alert.js
+++ docroot/themes/custom/limestone/js/alert.js
@@ -12,6 +12,8 @@
 
       var top_menu_first = $("#block-topmenu > ul.menu.dropdown > li:first-child");
 
+
+      // when clicking on close
       $(context).find('.alert-section a.close').bind('touchstart click', function (event) {
         event.stopPropagation();
         event.preventDefault();
@@ -21,9 +23,17 @@
         Cookies.set('alert', 'true', {
           expires: inHalfADay
         });
-        return false;
+        // when opening from red icon
+        $('#block-topmenu ul.menu.dropdown li.top-menu-alert-icon a').bind('touchstart click', function (event) {
+          event.stopPropagation();
+          event.preventDefault();
+          //console.log('in 2nd alert function');
+          alert_section.slideDown();
+          $("#block-topmenu ul li.top-menu-alert-icon").remove();
+          Cookies.remove('alert');
+        });
       });
-
+      // when opening from red icon
       $('#block-topmenu ul.menu.dropdown li.top-menu-alert-icon a').bind('touchstart click', function (event) {
         event.stopPropagation();
         event.preventDefault();
@@ -31,9 +41,8 @@
         alert_section.slideDown();
         $("#block-topmenu ul li.top-menu-alert-icon").remove();
         Cookies.remove('alert');
-        return false;
       });
-
+      // when loading page with closed cookie
       $('.alert-section', context).once('alertReadClosed').each(function () {
         // Apply the alertReadClosed effect to the elements only once.
         var closed_for_the_day = Cookies.get('alert');
@@ -47,7 +56,6 @@
             alert_section.slideUp();
             $("#block-topmenu ul li.top-menu-alert-icon a").remove();
             Cookies.remove('alert');
-            return false;
           });
         }
       });
